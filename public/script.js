bf2f394e51c198e97053c47b233bd6d7ed507c5d
Aikam Singh <aikamsingh07@gmail.com>
2024-08-12 09:14:14 -0700

    updated modal.

--- public/script.js
+++ public/script.js
@@ -1,43 +1,20 @@
-const http = require("http");
-const path = require("path");
-const fs = require('fs').promises;
+// Get the modal
+var modal = document.getElementById("myModal");
 
-const host = 'localhost';
-const port = 8000;
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
 
-const mimeTypes = {
-    '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'application/javascript',
-    '.png': 'image/png',
-    '.jpg': 'image/jpeg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-    '.json': 'application/json',
-    '.ico': 'image/x-icon'
-};
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-const requestListener = function (req, res) {
-    let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
-    let extname = String(path.extname(filePath)).toLowerCase();
-    let contentType = mimeTypes[extname] || 'application/octet-stream';
-
-    console.log(`Request for ${filePath}, Content-Type: ${contentType}`);
-
-    fs.readFile(filePath)
-    .then(contents => {
-        res.setHeader("Content-Type", contentType);
-        res.writeHead(200);
-        res.end(contents);
-    })
-    .catch(err => {
-        console.error(`Error serving ${filePath}: ${err}`);
-        res.writeHead(404);
-        res.end("File not found");
-    });
-};
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
